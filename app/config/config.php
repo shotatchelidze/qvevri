bd422411dfaa01fc2e6b7702e7bb60f0dad6cf9e
GeorgeQart <geofarmer71@gmail.com>
2020-05-18 19:55:59 +0400

    cahnge css

--- app/config/config.php
+++ app/config/config.php
@@ -2,15 +2,15 @@
   // DB Params
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
-  define('DB_PASS', '654321');
+  define('DB_PASS', '');
   define('DB_NAME', 'qvevri');
 
   // App Root
   define('APPROOT', (dirname(__FILE__, 2)));
   // URL Root
-  define('URLROOT', 'http://localhost:8012/qvevri');
+  define('URLROOT', 'http://localhost:/qvevri');
   // admin URL Root
-  define('URLROOT_ADMIN', 'http://localhost:8012/qvevri');
+  define('URLROOT_ADMIN', 'http://localhost:/qvevri');
   // Site Name
   define('SITENAME', 'Qvevri');
   // App Version
