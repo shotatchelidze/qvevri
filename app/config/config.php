382af6f1858f1e6cebf283a8b2add8210b75e5e9
Shota <shotikotchelidze@gmail.com>
2020-05-19 15:01:58 +0400

    ss

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '654321');
+  define('DB_NAME', 'qvevri');
+
+  // App Root
+  define('APPROOT', (dirname(__FILE__, 2)));
+  // URL Root
+  define('URLROOT', 'http://localhost:8012/qvevri');
+  // admin URL Root
+  define('URLROOT_ADMIN', 'http://localhost:8012/qvevri');
+  // Site Name
+  define('SITENAME', 'Qvevri'); 
+  // App Version
+  define('APPVERSION', '7.4.2');
